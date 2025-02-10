02f4a239191d619beb382edafb92bc244cdc7f0d
Jordi Perpiña <thejordiperpina@gmail.com>
2025-02-10 15:27:23 +0100

    He acabat el main i ara es crea els 10000 animals i fa un recompte de cada tipus d'animal

--- Animals/src/animals/Animals.java
+++ Animals/src/animals/Animals.java
@@ -21,39 +21,44 @@ public class Animals {
      */
     public static void main(String[] args) {
         //Constantes
-        final int MAX_ANIMALS=10000;
+        final int MAX_ANIMALS = 10000;
         // Variables
-        List<quatrePotes> llistaAnimals=new ArrayList<>();
-        
-        Random random= new Random();
-        
-        for(int i=0; i<MAX_ANIMALS;i++){
-        int numAleatori=random.nextInt(4)+1;
-        
-        switch(numAleatori){
-            case 1:
-                llistaAnimals.add(i, new quatrePotes(TIPUS.GOS));
-                break;
-            case 2:
-                llistaAnimals.add(i, new quatrePotes(TIPUS.ELEFANT));
-                break;
-            case 3:
-                llistaAnimals.add(i, new quatrePotes(TIPUS.GAT));
-                break;
-            case 4:
-                llistaAnimals.add(i, new quatrePotes(TIPUS.LLEO));
-                break;
+        List<quatrePotes> llistaAnimals = new ArrayList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < MAX_ANIMALS; i++) {
+            int numAleatori = random.nextInt(4) + 1;
+
+            switch (numAleatori) {
+                case 1:
+                    llistaAnimals.add(i, new quatrePotes(TIPUS.GOS));
+                    break;
+                case 2:
+                    llistaAnimals.add(i, new quatrePotes(TIPUS.ELEFANT));
+                    break;
+                case 3:
+                    llistaAnimals.add(i, new quatrePotes(TIPUS.GAT));
+                    break;
+                case 4:
+                    llistaAnimals.add(i, new quatrePotes(TIPUS.LLEO));
+                    break;
+            }
         }
-        }
-        for(quatrePotes animal:llistaAnimals){
+        for (quatrePotes animal : llistaAnimals) {
             System.out.println(animal);
         }
         
-        Map<TIPUS, Integer> qtAnimals=new HashMap<>();
-        
-        for(quatrePotes animal:llistaAnimals){
-            
+        Map<TIPUS, Integer> qtAnimals = new HashMap<>();
+        for (quatrePotes animal : llistaAnimals) {
+            TIPUS tipo = animal.getTipus(); // Obtener el tipo de animal
+            qtAnimals.put(tipo, qtAnimals.getOrDefault(tipo, 0) + 1); // Contar
+        }
+
+        // Imprimir resultados
+        for (Map.Entry<TIPUS, Integer> entry : qtAnimals.entrySet()) {
+            System.out.println("Tipus d'animal: " + entry.getKey() + " , quantitat: " + entry.getValue());
         }
     }
-    
+
 }
